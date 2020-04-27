d722aa1dc1d520130ba83f6fc97e45f537e06695
vc-dhavaljoshi <dhaval.joshi@viitor.cloud>
2020-04-27 13:11:18 +0530

    Service provider file update

--- src/AddressesServiceProvider.php
+++ src/AddressesServiceProvider.php
@@ -30,6 +30,9 @@ class AddressesServiceProvider extends ServiceProvider
     {   
         if (! class_exists('CreateAddressTable')) {
             $this->publishes([
+                __DIR__.'/../config/addressable.php' => config_path('addressable.php')
+            ],'address-config');
+            $this->publishes([
                 __DIR__.'/../database/migrations/create_address_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_address_table.php'),
             ], 'migrations');
         }
